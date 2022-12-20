67159947a7a05acdaae3b02760bb7fa01e9d97fc
Seniacat <senya.sidorovich@gmail.com>
2022-12-20 10:00:42 +0300

    removed makemigrations from entrypoint.sh

--- django_api/movies-admin/config/settings.py
+++ django_api/movies-admin/config/settings.py
@@ -45,9 +45,11 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
+
+if DEBUG:
+    MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware',]
 
 ROOT_URLCONF = 'config.urls'
 
